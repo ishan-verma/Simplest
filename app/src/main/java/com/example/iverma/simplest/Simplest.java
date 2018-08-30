e78c9d099e1da87155155971a7fdbc3212c92bdc
ishancringle <ishan@cringle.net>
2018-08-30 12:18:51 -0400

    first commit

---
+++ app/src/main/java/com/example/iverma/simplest/Simplest.java
@@ -0,0 +1,13 @@
+package com.example.iverma.simplest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Simplest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simplest);
+    }
+}
